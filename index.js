d98e37101dea13e6f94bf4a7f4cf67f1c71f6584
Robert <osipovitchr@gmail.com>
2022-04-25 11:54:03 -0400

    added HTIAA information to portfolio + Jquery for easy fadeIn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+$('section.intro').hide()
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -10,3 +11,6 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+
+$('section.intro').fadeIn(1000)
